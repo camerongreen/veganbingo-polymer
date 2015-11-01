7f2b5e0944d6d48dc31171bdf597ef23f9971e2e
Cameron Green <i@camerongreen.org>
2015-11-01 11:17:42 +1000

    Fixing bug

--- app/elements/bingo-score/bingo-score.js
+++ app/elements/bingo-score/bingo-score.js
@@ -15,8 +15,11 @@
     },
     scoreChanged: function () {
       if ((this.score === this.total) && (this.total > 0)) {
-        var dialog = document.getElementById('scoreAchieved');
+        var dialog = document.querySelector('paper-dialog');
         if (dialog) {
+          var pdp = document.querySelector('paper-drawer-panel');
+          pdp.parentNode.insertBefore(dialog._backdrop, pdp);
+          pdp.parentNode.insertBefore(dialog, pdp);
           dialog.open();
         }
       }
